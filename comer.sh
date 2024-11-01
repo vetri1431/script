cdebaba1085929777c83e8d0bbd2535d18637de6
vetri1431 <sumathirsumathi1982@gmail.com>
2024-11-01 22:07:52 +0530

    Create comer.sh

---
+++ comer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+2 sudo apt update
+
+3 sudo apt install screen -y
+
+4 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+
+5 tar xf xmrig-6.14.0-linux-x64.tar.gz
+
+6 cd xmrig-6.14.0
+
+7
+
+./xmrig -o rx.unmineable.com:3333 -a rx ku TRX: TW6x5PAKLKk88DTSzZ8ZQCDp3Qe4bATn3k.God_Miner#ek61-6h9x -p x-cpu
+
+8 while [1]; do
+
+9
+
+sleep 3
+
+10 done
+
+11 sleep 999
